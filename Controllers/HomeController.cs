9c74ebc5f11bf1c167a84ddd7bb6403eeb899aca
Earl Harris <earleharrisjr@gmail.com>
2022-10-04 15:06:43 -0400

    data searchable by state

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using CovidTrackingMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+
+
+
+namespace CovidTrackingMVC.Controllers
+{
+
+    public class HomeController : Controller
+    {
+    
+        //Hosted web API REST Service base url  
+        string Baseurl = "https://api.covidtracking.com/";
+
+        public async Task<ActionResult> Index()
+        {
+            List<CovidDataModel> CovData = new List<CovidDataModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("v1/states/current.json");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the list  
+                    CovData = JsonConvert.DeserializeObject<List<CovidDataModel>>(EmpResponse);
+
+                }
+                //returning the  list to view  
+                return View(CovData);
+            }
+        }
+
+
+
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    //sorting
+    public static class extensionmethods
+    {
+        public static IQueryable<T> OrderByPropertyName<T>(this IQueryable<T> q, string SortField, bool Ascending)
+        {
+            var param = Expression.Parameter(typeof(T), "p");
+            var prop = Expression.Property(param, SortField);
+            var exp = Expression.Lambda(prop, param);
+            string method = Ascending ? "OrderBy" : "OrderByDescending";
+            Type[] types = new Type[] { q.ElementType, exp.Body.Type };
+            var rs = Expression.Call(typeof(Queryable), method, types, q.Expression, exp);
+            return q.Provider.CreateQuery<T>(rs);
+        }
+    }
+
+}
---
+++ Models/CovidDataModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace CovidTrackingMVC.Models
+{    public class CovidDataModel
+    {
+        public int Date { get; set; }
+        public string State { get; set; }
+        public int Positive { get; set; }
+        public int? probableCases { get; set; }
+        public int? Negative { get; set; }
+        public int? pending { get; set; }
+        public string totalTestResultsSource { get; set; }
+        public int totalTestResults { get; set; }
+        public int? hospitalizedCurrently { get; set; }
+        public int? hospitalizedCumulative { get; set; }
+        public int? inIcuCurrently { get; set; }
+        public int? inIcuCumulative { get; set; }
+        public int? onVentilatorCurrently { get; set; }
+        public int? onVentilatorCumulative { get; set; }
+        public int? recovered { get; set; }
+        public string lastUpdateEt { get; set; }
+        public DateTime? dateModified { get; set; }
+        public string checkTimeEt { get; set; }
+        public int death { get; set; }
+        public int? Hospitalized { get; set; }
+        public int? hospitalizedDischarged { get; set; }
+        public DateTime? dateChecked { get; set; }
+        public int? totalTestsViral { get; set; }
+        public int? positiveTestsViral { get; set; }
+        public int? negativeTestsViral { get; set; }
+        public int? positiveCasesViral { get; set; }
+        public int? deathConfirmed { get; set; }
+        public int? deathProbable { get; set; }
+        public int? totalTestEncountersViral { get; set; }
+        public int? totalTestsPeopleViral { get; set; }
+        public int? totalTestsAntibody { get; set; }
+        public int? positiveTestsAntibody { get; set; }
+        public int? negativeTestsAntibody { get; set; }
+        public int? totalTestsPeopleAntibody { get; set; }
+        public int? positiveTestsPeopleAntibody { get; set; }
+        public int? negativeTestsPeopleAntibody { get; set; }
+        public int? totalTestsPeopleAntigen { get; set; }
+        public int? positiveTestsPeopleAntigen { get; set; }
+        public int? totalTestsAntigen { get; set; }
+        public int? positiveTestsAntigen { get; set; }
+        public string fips { get; set; }
+        public int positiveIncrease { get; set; }
+        public int negativeIncrease { get; set; }
+        public int Total { get; set; }
+        public int totalTestResultsIncrease { get; set; }
+        public int posNeg { get; set; }
+        public object dataQualityGrade { get; set; }
+        public int deathIncrease { get; set; }
+        public int hospitalizedIncrease { get; set; }
+        public string hash { get; set; }
+        public int commercialScore { get; set; }
+        public int negativeRegularScore { get; set; }
+        public int negativeScore { get; set; }
+        public int positiveScore { get; set; }
+        public int score { get; set; }
+        public string grade { get; set; }
+    }
+
+}
